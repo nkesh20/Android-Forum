2845a566b70001788e7b57815a60b0d8705c21bb
nkesh20 <nkesh20@freeuni.edu.ge>
2024-07-08 11:42:05 +0400

    delete extra imports
    Signed-off-by: nkesh20 <nkesh20@freeuni.edu.ge>

--- app/src/main/java/com/example/forcedsocial/auth/AuthRepository.kt
+++ app/src/main/java/com/example/forcedsocial/auth/AuthRepository.kt
@@ -1,7 +1,3 @@
-import android.app.Activity
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.google.firebase.auth.FirebaseAuth
 
 class AuthRepository(private val auth: FirebaseAuth) {
