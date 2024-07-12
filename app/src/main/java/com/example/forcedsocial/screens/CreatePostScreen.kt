f0bbdbc7f4e0586683b730b6b0413212ee5a8809
Saba Khutsishvili <skhutsishvili@onairent.live>
2024-07-12 21:00:14 +0400

    Persist topicId on post creation

--- app/src/main/java/com/example/forcedsocial/screens/CreatePostScreen.kt
+++ app/src/main/java/com/example/forcedsocial/screens/CreatePostScreen.kt
@@ -1,5 +1,6 @@
 package com.example.forcedsocial.screens
 
+import android.net.Uri
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -18,7 +19,7 @@ import com.example.forcedsocial.viewmodels.PostViewModel
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun CreatePostScreen(navController: NavController, authViewModel: AuthViewModel) {
+fun CreatePostScreen(navController: NavController, authViewModel: AuthViewModel, topicId: String) {
     val postViewModel: PostViewModel = viewModel()
     val content = remember { mutableStateOf("") }
     val user = FirebaseAuth.getInstance().currentUser
@@ -50,7 +51,7 @@ fun CreatePostScreen(navController: NavController, authViewModel: AuthViewModel)
                 Button(onClick = {
                     user?.let {
                         val userId = it.uid
-                        postViewModel.createPost(userId, content.value, imageUri.value, "", context)
+                        postViewModel.createPost(userId, content.value, imageUri.value, topicId, context)
                     }
                 }) {
                     Text(text = "Post")
